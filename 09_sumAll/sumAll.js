35dee11462959835b68f1ebf5f6e7c6225936a2e
Cactuss26 <179231437+Cactuss26@users.noreply.github.com>
2026-01-11 01:00:58 +0530

    Finish

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || (String(num1).includes(".")) || (String(num2).includes(".")) ||
+         typeof num1 !== "number" || typeof num2 !== "number"
+    ) {
+        return "ERROR";
+    }
 
+    if (num1 >= num2) {
+        return (num1 * (num1 + 1) / 2) - ((num2 - 1) * num2 / 2); 
+    }
+
+    else {
+        return (num2 * (num2 + 1) / 2) - ((num1 - 1) * num1 / 2);
+    }
 };
 
 // Do not edit below this line
