957e699c6abc35d0e9ef89844bed31c18bf5f495
Cactuss26 <179231437+Cactuss26@users.noreply.github.com>
2026-01-11 21:44:48 +0530

    Finish

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  convertedTemp = ((temp - 32) * (5 / 9))
+  convertedTemp = Math.round(convertedTemp * 10);
+  return convertedTemp / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  convertedTemp = (temp * (9 / 5)) + 32;
+  convertedTemp = Math.round(convertedTemp * 10);
+  return convertedTemp / 10;
 };
 
 // Do not edit below this line
