ac1badf4e0e1ba0c969f5d9a3e684c9be804edcd
Cactuss26 <179231437+Cactuss26@users.noreply.github.com>
2026-01-21 00:00:38 +0530

    Finish

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    for (person of people) {
+        if (!("yearOfDeath" in person)) {
+            currentDate = new Date();
+            person.yearOfDeath = Number(currentDate.getFullYear());
+        }
+    }
 
+    people = people.map(person => {
+        const obj = {
+            name: person.name,
+            age: person.yearOfDeath - person.yearOfBirth,
+        }
+        return obj;
+    })
+
+    people.sort((a, b) => b.age - a.age);
+
+    return people[0];
 };
 
 // Do not edit below this line
